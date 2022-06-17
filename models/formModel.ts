1a2aa61f16fc6e31a9bac646b980925eeebc0731
Qelkor <103414801+Qelkor@users.noreply.github.com>
2022-06-17 14:37:38 +0800

    last

--- models/formModel.ts
+++ models/formModel.ts
@@ -1,41 +1,43 @@
 import { Schema, model } from "mongoose";
 
 interface RoomOptions {
-	package: string;
-	theme: string[];
+  package: string;
+  theme: string[];
 }
 
 interface Selection {
-	roomName: string;
-	roomOptions: RoomOptions;
+  roomName: string;
+  roomOptions: RoomOptions;
 }
 
 interface IForm {
-	propertyType: string;
-	propertyStatus: string;
-	renoType: string;
-	renoPriority: string;
-	keyCollected: boolean;
-	loanRequired: boolean;
-	rooms: string[];
-	budget: number;
-	floorSize: number;
-	comments: string;
-	selection: [Selection];
+  propertyType: string;
+  propertyStatus: string;
+  renoType: string;
+  renoPriority: string;
+  keyCollected: boolean;
+  loanRequired: boolean;
+  rooms: string[];
+  budget: number;
+  floorSize: number;
+  comments: string;
+  selection: [Selection];
 }
 
 const formSchema = new Schema<IForm>({
-	propertyType: String,
-	propertyStatus: String,
-	renoType: String,
-	renoPriority: String,
-	keyCollected: Boolean,
-	loanRequired: Boolean,
-	rooms: [String],
-	budget: Number,
-	floorSize: Number,
-	comments: String,
-	selection: [{ roomName: String, roomOptions: { package: String, theme: [String] } }],
+  propertyType: String,
+  propertyStatus: String,
+  renoType: String,
+  renoPriority: String,
+  keyCollected: Boolean,
+  loanRequired: Boolean,
+  rooms: [String],
+  budget: Number,
+  floorSize: Number,
+  comments: String,
+  selection: [
+    { roomName: String, roomOptions: { package: String, theme: [String] } },
+  ],
 });
 
 const Form = model<IForm>("FormModel", formSchema);
