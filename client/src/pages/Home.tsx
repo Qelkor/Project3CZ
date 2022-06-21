fe4bdf681f5a6180cce5e4e0ea6e0bd46d82044e
Qelkor <103414801+Qelkor@users.noreply.github.com>
2022-06-21 10:37:46 +0800

    searchMKI

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,9 +1,27 @@
 import React from 'react'
+//import { useState, useEffect} from "react";
 
 const Home = () => {
+  // const [searchText, setSearchText] = useState("");
+
+  // function handleChange(event: React.FormEvent<HTMLInputElement>) {
+  //   event.preventDefault();
+  //   setSearchText(event.target.value);
+    
+  // }
+
+  
+
+
+
+
   return (
     <div>
-      <p>Search bar</p>
+      {/* <form >
+    <div className="InputContainer">
+        <input value={searchText} onChange={handleChange} type="text" placeholder="Search films..." className="SearchInput" />
+      </div>
+      </form> */}
       <p>search results</p>
 
     </div>
