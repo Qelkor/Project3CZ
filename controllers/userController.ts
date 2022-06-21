113c3dbf433127a17c4cffe9f00ebb5efb9fae4d
Qelkor <103414801+Qelkor@users.noreply.github.com>
2022-06-21 15:04:58 +0800

    clash

--- controllers/userController.ts
+++ controllers/userController.ts
@@ -19,40 +19,40 @@ const salt = bcrypt.genSaltSync(10);
 // Register Routes
 Router.post("/signup", async (req: Request, res: Response) => {
   const body = req.body;
+  body.password = await bcrypt.hash(body.password, salt);
   try {
     const newUser = await Users.create(body);
-    newUser.password = await bcrypt.hash(newUser.password, salt);
     res.status(200).send({ status: "Great Success", data: newUser });
   } catch (err: any) {
     res.status(400).send({ status: "Fail", error: err.message });
   }
 });
 
-Router.get("/seed", async (req, res) => {
-  try {
-    const seed = await Users.create({
-      name: "Chris",
-      email: "telkor@helloworld.com",
-      password: "helloworld",
-      userForm: ["62addf7945f7bd06e830fbdd", "62ade48375185e59d7106c58"], //Object ID of Forms belonging to this user
-      propertyType: "HDB",
-      propertyStatus: "Exisiting",
-      renoType: "Partial",
-      renoPriority: "Stick to budget",
-      keyCollected: true,
-      loanRequired: false,
-      rooms: ["62adcb4e6c4a3126663f88c0", "62adcb4e6c4a3126663f88c1"],
-      budget: 10000,
-    });
-    res.send(seed);
-  } catch (err) {
-    res.send(err);
-  }
-});
+// Router.get("/seed", async (req, res) => {
+//   try {
+//     const seed = await Users.create({
+//       name: "Chris",
+//       email: "telkor@helloworld.com",
+//       password: "helloworld",
+//       userForm: ["62addf7945f7bd06e830fbdd", "62ade48375185e59d7106c58"], //Object ID of Forms belonging to this user
+//       propertyType: "HDB",
+//       propertyStatus: "Exisiting",
+//       renoType: "Partial",
+//       renoPriority: "Stick to budget",
+//       keyCollected: true,
+//       loanRequired: false,
+//       rooms: ["62adcb4e6c4a3126663f88c0", "62adcb4e6c4a3126663f88c1"],
+//       budget: 10000,
+//     });
+//     res.send(seed);
+//   } catch (err) {
+//     res.send(err);
+//   }
+// });
 
 // // Login User
-Router.get("/login", async (req: Request, res: Response) => {
-  const { name, password } = req.body;
+Router.post("/login", async (req: Request, res: Response) => {
+  const { email, password } = req.body;
   const user = await Users.findOne({ name });
   if (!user || !bcrypt.compareSync(password, user.password)) {
     res.status(400).send({ message: "Access Denied!" });
