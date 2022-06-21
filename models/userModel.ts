601c728438af933e2c176e324a920873dc7b18ad
wongzl <zhenglong.wong@gmail.com>
2022-06-21 10:33:15 +0800

    fixed signup problem

--- models/userModel.ts
+++ models/userModel.ts
@@ -20,7 +20,7 @@ export interface IUser {
 
 const userSchema = new Schema<IUser>({
   name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
+  email: { type: String, required: true},
   password: { type: String, required: true },
   userForm: [{ type: Schema.Types.ObjectId, ref: formModelName }],
   propertyType: String,
