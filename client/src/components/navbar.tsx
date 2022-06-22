c947749a258cb7a116ba30d290d0e2e7dff099f4
Qelkor <103414801+Qelkor@users.noreply.github.com>
2022-06-23 00:21:18 +0800

    muibueno

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -2,34 +2,208 @@ import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useAtom } from "jotai";
 import { userAtom } from "../App";
-
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import AdbIcon from "@mui/icons-material/Adb";
+import React from "react";
 
 interface LogoutResponse {
   status: string;
 }
 
 const Navbar = () => {
-	const [user, setUser] = useAtom(userAtom);
-	const navigate = useNavigate();
+  const [user, setUser] = useAtom(userAtom);
+  const navigate = useNavigate();
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
+    null
+  );
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
+    null
+  );
 
-	const Logout = async () => {
-		const { data } = await axios.get<LogoutResponse>("/api/user/logout");
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  const Logout = async () => {
+    const { data } = await axios.get<LogoutResponse>("/api/user/logout");
     alert(data.status);
     setUser(undefined);
-    navigate("/")
-	};
+    navigate("/");
+  };
 
-	return (
-		<>
-			<nav>
-				<Link to="/">Home</Link>
-				<Link to="/login">Login</Link>
-				<Link to="/loginTest">Login Test</Link>
+  return (
+    <>
+      <nav>
+        <AppBar position="static">
+          <Container maxWidth="xl">
+            <Toolbar disableGutters>
+              <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
+              <Typography
+                variant="h6"
+                noWrap
+                component="a"
+                href="/"
+                sx={{
+                  mr: 2,
+                  display: { xs: "none", md: "flex" },
+                  fontFamily: "monospace",
+                  fontWeight: 700,
+                  letterSpacing: ".3rem",
+                  color: "inherit",
+                  textDecoration: "none",
+                }}
+              >
+                CREDO
+              </Typography>
+
+              <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+                <IconButton
+                  size="large"
+                  aria-label="account of current user"
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                  onClick={handleOpenNavMenu}
+                  color="inherit"
+                >
+                  <MenuIcon />
+                </IconButton>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorElNav}
+                  anchorOrigin={{
+                    vertical: "bottom",
+                    horizontal: "left",
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: "top",
+                    horizontal: "left",
+                  }}
+                  open={Boolean(anchorElNav)}
+                  onClose={handleCloseNavMenu}
+                  sx={{
+                    display: { xs: "block", md: "none" },
+                  }}
+                >
+                  <MenuItem key={"Home"} onClick={handleCloseNavMenu}>
+                    <Typography textAlign="center">
+                      <Link to="/">{"Home"}</Link>
+                    </Typography>
+                  </MenuItem>
+                  <MenuItem key={"login"} onClick={handleCloseNavMenu}>
+                    <Typography textAlign="center">
+                      <Link to="/login">{"Login"}</Link>
+                    </Typography>
+                  </MenuItem>
+                </Menu>
+              </Box>
+              <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
+              <Typography
+                variant="h5"
+                noWrap
+                component="a"
+                href=""
+                sx={{
+                  mr: 2,
+                  display: { xs: "flex", md: "none" },
+                  flexGrow: 1,
+                  fontFamily: "monospace",
+                  fontWeight: 700,
+                  letterSpacing: ".3rem",
+                  color: "inherit",
+                  textDecoration: "none",
+                }}
+              >
+                LOGO
+              </Typography>
+              <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+                <Button
+                  key={"home"}
+                  onClick={handleCloseNavMenu}
+                  sx={{ my: 2, color: "white", display: "block" }}
+                >
+                  <Link to="/">{"Home"}</Link>
+                </Button>
+
+                {!user ? (
+                  <Button
+                    key={"Login"}
+                    onClick={handleCloseNavMenu}
+                    sx={{ my: 2, color: "white", display: "block" }}
+                  >
+                    <Link to="/login">{"Login"}</Link>
+                  </Button>
+                ) : null}
+              </Box>
+              {user ? (
+                <Box sx={{ flexGrow: 0 }}>
+                  <Tooltip title="Open settings">
+                    <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                      <Avatar
+                        alt="Remy Sharp"
+                        src="/static/images/avatar/2.jpg"
+                      />
+                    </IconButton>
+                  </Tooltip>
+                  <Menu
+                    sx={{ mt: "45px" }}
+                    id="menu-appbar"
+                    anchorEl={anchorElUser}
+                    anchorOrigin={{
+                      vertical: "top",
+                      horizontal: "right",
+                    }}
+                    keepMounted
+                    transformOrigin={{
+                      vertical: "top",
+                      horizontal: "right",
+                    }}
+                    open={Boolean(anchorElUser)}
+                    onClose={handleCloseUserMenu}
+                  >
+                    <MenuItem key={"logout"} onClick={handleCloseUserMenu}>
+                      <Typography textAlign="center">
+                        <button onClick={Logout}>{"Logout"}</button>
+                      </Typography>
+                    </MenuItem>
+                  </Menu>
+                </Box>
+              ) : null}
+            </Toolbar>
+          </Container>
+        </AppBar>
+
+        {/* <nav>
+        <Link to="/">Home</Link>
+        <Link to="/login">{"Login"}</Link>
+        <Link to="/loginTest">Login Test</Link>
         <Link to="/signup"> Sign up</Link>
-        <button onClick={Logout}>Logout</button>
-			</nav>
-		</>
-	);
+        <button onClick={Logout}>Logout</button> */}
+      </nav>
+    </>
+  );
 };
 
 export default Navbar;
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,39 +1,98 @@
-import React from 'react'
-//import { useState, useEffect} from "react";
-import Navbar from "../components/navbar"
-import {useAtom} from 'jotai'
-import {userAtom} from '../App'
+import React from "react";
+import { useState, useEffect } from "react";
+import Navbar from "../components/navbar";
+import { useAtom } from "jotai";
+import { userAtom } from "../App";
+import axios from "axios";
+import { response } from "express";
+import { IVendor } from "../../../models/vendorModel";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+import Autocomplete, { createFilterOptions } from "@mui/material/Autocomplete";
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
+
+const filter = createFilterOptions<IVendor>();
 
 const Home = () => {
+  const [user, setUser] = useAtom(userAtom);
+  const [vendors, setVendors] = useState([]);
+  const [value, setValue] = React.useState<IVendor | null>(null);
 
-  const[user, setUser] = useAtom(userAtom)
-  // const [searchText, setSearchText] = useState("");
+  useEffect(() => {
+    axios.get(`/api/vendor/`).then((res) => {
+      const data = res.data;
+      setVendors(data);
+      console.log(data);
+    });
+  }, []);
 
-  // function handleChange(event: React.FormEvent<HTMLInputElement>) {
-  //   event.preventDefault();
-  //   setSearchText(event.target.value);
-    
-  // }
-
-  
-
-
-
+  if (vendors.length === 0) {
+    return (
+      <Box sx={{ display: "flex", justifyContent: "center" }}>
+        <CircularProgress />
+      </Box>
+    );
+  }
 
   return (
     <div>
       <Navbar />
-      {/* <form >
-    <div className="InputContainer">
-        <input value={searchText} onChange={handleChange} type="text" placeholder="Search films..." className="SearchInput" />
-      </div>
-      </form>
-      <p>search results</p> */}
-      Homepage
-      <h2>{`${user}`}</h2>
+      <Autocomplete
+        value={value}
+        filterOptions={(options, params) => {
+          const filtered = filter(options, params);
+
+          const { inputValue } = params;
+          // Suggest the creation of a new value
+          const isExisting = options.some(
+            (option) => inputValue === option.name
+          );
+
+          return filtered;
+        }}
+        selectOnFocus
+        clearOnBlur
+        handleHomeEndKeys
+        id="free-solo-with-text-demo"
+        options={vendors}
+        getOptionLabel={(option) => {
+          // Value selected with enter, right from the input
+          if (typeof option === "string") {
+            return option;
+          }
+          // Regular option
+          return option.name;
+        }}
+        renderOption={(props, option) => <li {...props}>{option.name}</li>}
+        sx={{ width: 300 }}
+        freeSolo
+        renderInput={(params) => (
+          <TextField {...params} label="Enter Interior Designer name" />
+        )}
+      />
+      {vendors?.map((vendorId: IVendor) => (
+        <Card sx={{ maxWidth: 345 }}>
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {`Vendor: ${vendorId.name}`}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {`Description: ${vendorId.description}`}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">{`Certifications: ${vendorId.certs}`}</Button>
+            {user ? <Button size="small">Learn More</Button> : null}
+          </CardActions>
+        </Card>
+      ))}
     </div>
-    
-  )
-}
+  );
+};
 
 export default Home;
